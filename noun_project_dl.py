b9c109fec277038a6029e2096b1e3e8366278f3a
Chris L <chris.luginbuhl@gmail.com>
2019-03-29 11:48:45 -0400

    Records icon attributions
    must specify -outpath as command line parameter. Adjust number of icons to get in constants, and keyword/image type in request URL

--- noun_project_dl.py
+++ noun_project_dl.py
@@ -1,6 +1,6 @@
 
 #! python3
-# noun_project_dl.py - Downloads every single APOD image, excludes non-image files.
+# noun_project_dl.py - Downloads icons and their attributions from noun project.
 # Chris Luginbuhl Mar 2019
 
 import json
@@ -9,28 +9,41 @@ import os
 import time
 from tqdm import tqdm
 import argparse
+from requests_oauthlib import OAuth1
+import noun_project_config
 
-#source path of json, with trailing slash
+NUM_REQUESTS = 40     #API returns up to 50 results. How many times to request
+START_OFFSET = 400   #Offset for filename and for API request, skipping images already acquired
+URLS_PER_REQUEST = 50
+auth = OAuth1(noun_project_config.api_key, noun_project_config.api_secret)
+endpoint = "https://api.thenounproject.com/icons/drone?limit=50&offset=" #this API returns up to 50 results
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--infile", help="Path to json from nounproject")
+parser.add_argument("-i", "--infile", help="Path to json from nounproject", default = None)
 parser.add_argument("-o", "--outpath", help="Path to folder of downloaded images (will be created if it doesn't exist)")
-parser.add_argument("-s", "--final_size", help="Size in pixels of resized images (square)", type=int)
-parser.add_argument("-c", "--cropmode", help="Crop mode, 'f' or 'i' for Fit or fIll", default = 'f')
+parser.add_argument("-s", "--finalsize", help="Size in pixels of resized images (square)", type=int)
 args = parser.parse_args()
 
-print( "Infile {}, Output Path {}, Final Size {}, Crop Mode {} ".format(
+print( "Infile {}, Output Path {}, Final Size {}".format(
         args.infile,
         args.outpath,
-        args.final_size,
-        args.cropmode
+        args.finalsize,
         ))
 
-txt = open(args.infile)
-with open(args.infile, 'r') as f:
-    nouns_dict = json.load(f)
 
 
+def get_json(offset):
+    if args.infile != None:
+        txt = open(args.infile)
+        with open(args.infile, 'r') as f:
+            nouns = json.load(f) #dictionary type
+    else:
+        response = requests.get(endpoint + str(offset), auth=auth)
+        nouns = response.json() #dictionary type
+
+    return(nouns)
+
+# end extract_json()
 
 # this function came from https://hackersandslackers.com/extract-data-from-complex-json-python/
 def extract_values(obj, key):
@@ -52,24 +65,41 @@ def extract_values(obj, key):
 
     results = extract(obj, arr, key)
     return results
+# end extract_values
 
-URL_values = extract_values(nouns_dict, 'preview_url')  #2nd parameter is the key that gives the URL
-print("Found ", len(URL_values), " URLs.")
+def get_images(URLs, counter):
+    for noun_URL in tqdm(URLs):
+        if noun_URL is None:
+            print('Could not find image.')
+        else:
+            # print('Downloading image %s...' % (noun_URL))
+            resp = requests.get(noun_URL)
+            resp.raise_for_status()
+            #save the file
+            imageFile = open(os.path.join(args.outpath, 'drone_' + str(counter)) + '.png', 'wb')
+            for chunk in resp.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+            counter += 1
+            time.sleep(0.15)  #prevents flooding the server with download activity. Int or float as argument
+#end get_images
+
+def record_attribution(names_list):
+    attribFile = open(os.path.join(args.outpath, 'attribution' + '.txt'), 'a')  # append mode
+    for name in names_list:
+        attribFile.write(name + '\n')
+        #print(name)
+    attribFile.close()
+# end record_attribution()
+
 os.makedirs(args.outpath, exist_ok=True)                         # store imgs in subfolder
-
-counter = 0
-for noun_URL in tqdm(URL_values):
-    if noun_URL is None:
-        print('Could not find image.')
-    else:
-        # print('Downloading image %s...' % (noun_URL))
-        resp = requests.get(noun_URL)
-        resp.raise_for_status()
-        #save the file
-        imageFile = open(os.path.join(args.outpath, 'drone_' + str(counter)), 'wb')
-        for chunk in resp.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
-        counter += 1
-        time.sleep(1)  #prevents flooding the server with download activity. Int or float as argument
-print('Done.')
+for i in range(NUM_REQUESTS):
+    nouns_dict = get_json(START_OFFSET + i * URLS_PER_REQUEST)
+    URL_values = extract_values(nouns_dict, 'preview_url')  #2nd parameter is the dictionary key that gives the URL
+    names_list = extract_values(nouns_dict, 'attribution')
+    print("Found ", len(URL_values), " URLs.")
+    get_images(URL_values, START_OFFSET + i * URLS_PER_REQUEST)
+    record_attribution(names_list)
+    if ((args.infile == None) & (len(URL_values) != URLS_PER_REQUEST)):
+        break
+print("Done.")
